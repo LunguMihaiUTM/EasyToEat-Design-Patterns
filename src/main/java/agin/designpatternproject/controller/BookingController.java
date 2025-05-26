307e43eb669717aab4da89a52d58d4c262ba3248
Lungu Mihai <lungu2003mihai1@mail.ru>
2025-05-26 23:26:17 +0300

    Added new endpoints

--- src/main/java/agin/designpatternproject/controller/BookingController.java
+++ src/main/java/agin/designpatternproject/controller/BookingController.java
@@ -2,11 +2,19 @@ package agin.designpatternproject.controller;
 
 import agin.designpatternproject.dto.request.BookingDTO;
 import agin.designpatternproject.entity.Booking;
+import agin.designpatternproject.entity.User;
+import agin.designpatternproject.enums.Role;
+import agin.designpatternproject.exception.UserException;
 import agin.designpatternproject.facade.BookingFacade;
 import agin.designpatternproject.service.BookingService;
+import agin.designpatternproject.util.TokenExtractor;
+import agin.designpatternproject.util.UserExtractor;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/booking")
@@ -14,15 +22,59 @@ import org.springframework.web.bind.annotation.*;
 public class BookingController {
     private final BookingService bookingService;
     private final BookingFacade bookingFacade;
+    private final UserExtractor userExtractor;
+    private final TokenExtractor tokenExtractor;
 
     @GetMapping("/reorder")
     public ResponseEntity<String> reorder(@RequestParam Long bookingId){
         return ResponseEntity.ok(bookingService.reorder(bookingId));
     }
 
+
+    @SecurityRequirement(name = "bearerAuth")
     @PostMapping("/create")
-    public ResponseEntity<String> create(@RequestBody BookingDTO bookingDTO, @RequestParam String promoCode){
-        return ResponseEntity.ok(bookingFacade.createBooking(bookingDTO, promoCode));
+    public ResponseEntity<String> create(
+            @RequestBody BookingDTO bookingDTO,
+            @RequestParam String promoCode,
+            @RequestHeader("Authorization") String authHeader){
+
+        User user = userExtractor.getUser(tokenExtractor.getToken(authHeader));
+
+        return ResponseEntity.ok(bookingFacade.createBooking(user.getId(), bookingDTO, promoCode));
     }
+
+    @SecurityRequirement(name = "bearerAuth")
+    @PostMapping("/user-bookings")
+    public ResponseEntity<List<Booking>> getUserBookings(
+            @RequestHeader("Authorization") String authHeader){
+        User user = userExtractor.getUser(tokenExtractor.getToken(authHeader));
+        return ResponseEntity.ok(bookingService.getUserBookings(user.getId()));
+    }
+
+    @SecurityRequirement(name = "bearerAuth")
+    @PutMapping("/{bookingId}/next")
+    public ResponseEntity<String> moveToNext(
+            @PathVariable Long bookingId,
+            @RequestHeader("Authorization") String authHeader) {
+
+        User user = userExtractor.getUser(tokenExtractor.getToken(authHeader));
+        if(user.getRole()!= Role.ADMIN)
+            throw new UserException("You are not allowed to move this booking");
+        return ResponseEntity.ok(bookingService.nextState(bookingId));
+    }
+
+    @SecurityRequirement(name = "bearerAuth")
+    @PutMapping("/{bookingId}/cancel")
+    public ResponseEntity<String> cancel(
+            @PathVariable Long bookingId,
+            @RequestHeader("Authorization") String authHeader) {
+
+        User user = userExtractor.getUser(tokenExtractor.getToken(authHeader));
+        if(user.getRole()!= Role.ADMIN)
+            throw new UserException("You are not allowed to move this booking");
+        return ResponseEntity.ok(bookingService.cancelBooking(bookingId));
+    }
+
+
 
 }
---
+++ src/main/java/agin/designpatternproject/controller/RestaurantResourceController.java
@@ -0,0 +1,76 @@
+package agin.designpatternproject.controller;
+
+import agin.designpatternproject.dto.RestaurantDTO;
+import agin.designpatternproject.dto.request.RestaurantFilterDTO;
+import agin.designpatternproject.exception.BookingException;
+import agin.designpatternproject.exception.RestaurantResourceException;
+import agin.designpatternproject.service.RestaurantResourceService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/restaurant-resource")
+public class RestaurantResourceController {
+    private final RestaurantResourceService restaurantResourceService;
+
+    @PostMapping
+    @Operation(summary = "Get Restaurants by filter(categoryID, name)", description = "Return a List of RestaurantDTO, " +
+            " to get all restaurants make the request with null param")
+    public ResponseEntity<List<RestaurantDTO>> getRestaurants(@RequestBody(required = false) RestaurantFilterDTO filter) {
+        return ResponseEntity.ok(restaurantResourceService.getRestaurants(filter));
+    }
+
+    @GetMapping("/menu/{restaurantId}")
+    @Operation(summary = "Get Menu by Restaurant Id", description = "Return a MenuDTO")
+    public ResponseEntity<?> getMenuByRestaurantId(@PathVariable Long restaurantId) {
+        try {
+            return ResponseEntity.ok(restaurantResourceService.getMenuByRestaurantId(restaurantId));
+        } catch (RestaurantResourceException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.internalServerError().body("Service currently unavailable, please try a little later!");
+        }
+    }
+
+    @GetMapping("/restaurants/has-free-table/{locationId}")
+    @Operation(summary = "Verify if has free table", description = "Return a boolean var")
+    public ResponseEntity<?> hasFreeTable(@PathVariable Long locationId) {
+        try {
+            return ResponseEntity.ok(restaurantResourceService.isFreeTable(locationId));
+        } catch (BookingException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.internalServerError().body("Service currently unavailable, please try a little later!");
+        }
+    }
+
+
+    @GetMapping("/locations/{restaurantId}")
+    @Operation(summary = "Get locations by Restaurant Id", description = "Return a list of LocationDTO")
+    public ResponseEntity<?> getLocationsByRestaurantId(@PathVariable Long restaurantId) {
+        try {
+            return ResponseEntity.ok(restaurantResourceService.getLocationsByRestaurantId(restaurantId));
+        } catch (RestaurantResourceException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.internalServerError().body("Service currently unavailable, please try a little later!");
+        }
+    }
+
+    @GetMapping("/tables/{locationId}")
+    @Operation(summary = "Get free tables by Location Id", description = "Returns free tables based of a location " +
+            " based by locationId")
+    public ResponseEntity<?> getFreeTablesByLocationId(@PathVariable Long locationId) {
+        return ResponseEntity.ok(restaurantResourceService.getFreeTablesByLocationId(locationId));
+    }
+}
