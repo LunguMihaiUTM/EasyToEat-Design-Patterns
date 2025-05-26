dc1f224d5c86c30c01b446c26df9d1d163671888
Lungu Mihai <lungu2003mihai1@mail.ru>
2025-05-26 23:27:07 +0300

    small changes

--- src/main/java/agin/designpatternproject/facade/BookingFacade.java
+++ src/main/java/agin/designpatternproject/facade/BookingFacade.java
@@ -1,18 +1,24 @@
 package agin.designpatternproject.facade;
 
+import agin.designpatternproject.command.MarkTableAsOccupiedCommand;
+import agin.designpatternproject.command.TableAvailabilityCommand;
 import agin.designpatternproject.decorator.Discount;
 import agin.designpatternproject.dto.request.BookingDTO;
 import agin.designpatternproject.entity.Booking;
-import agin.designpatternproject.entity.Item;
+import agin.designpatternproject.entity.Table;
+import agin.designpatternproject.entity.User;
 import agin.designpatternproject.enums.Status;
 import agin.designpatternproject.exception.RestaurantResourceException;
 import agin.designpatternproject.factory.DiscountFactory;
+import agin.designpatternproject.observer.BookingObserver;
+import agin.designpatternproject.repository.TableRepository;
+import agin.designpatternproject.repository.UserRepository;
 import agin.designpatternproject.service.RestaurantResourceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
+
 
 @Service
 @RequiredArgsConstructor
@@ -23,8 +29,12 @@ public class BookingFacade {
     private final PriceCalculationService priceCalculationService;
     private final BookingPersistenceService bookingPersistenceService;
     private final RestaurantResourceService restaurantResourceService;
+    private final BookingObserver bookingObserver;
 
-    public String createBooking(BookingDTO dto, String promoCode) {
+    private final UserRepository userRepository;
+    private final TableRepository tableRepository;
+
+    public String createBooking(Long userId ,BookingDTO dto, String promoCode) {
 
         tableAvailabilityService.validateTableIsFree(dto.getTableId());
 
@@ -36,10 +46,13 @@ public class BookingFacade {
         Discount discount = DiscountFactory.getDiscountByPromoCode(promoCode);
         double finalPrice = discount.applyDiscount(totalPrice);
 
+
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new RestaurantResourceException("User not found")
+        );
+
         Booking booking = Booking.builder()
-                .name(dto.getName())
-                .phoneNumber(dto.getPhoneNumber())
-                .mail(dto.getMail())
+                .user(user)
                 .locationId(dto.getLocationId())
                 .tableId(dto.getTableId())
                 .noPeople(dto.getNoPeople())
@@ -48,6 +61,16 @@ public class BookingFacade {
                 .bookingStatus(Status.IN_PROGRESS)
                 .orderTime(LocalDateTime.now())
                 .build();
+
+
+        //Command
+        Table table = tableRepository.findById(dto.getTableId())
+                .orElseThrow(() -> new RestaurantResourceException("Table not found"));
+        TableAvailabilityCommand markAsOccupied = new MarkTableAsOccupiedCommand(table, tableRepository);
+        markAsOccupied.execute();
+
+        //Observer - notify the user
+        //bookingObserver.onBookingCreation(user, booking);
 
         bookingPersistenceService.saveBooking(booking);
         return "Booking successfully created";
